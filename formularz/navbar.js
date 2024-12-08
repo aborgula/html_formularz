a39a93cb39253c0b6abcd2589831b2bbf3fe8286
aborgula <132065514+aborgula@users.noreply.github.com>
2024-12-08 15:30:07 +0200

    Add files via upload

---
+++ formularz/navbar.js
@@ -0,0 +1,7 @@
+let navbar =
+    '<a href="lab01.html"><div class="menuopt">Home</div></a>\n' +
+    '<a href="mapa.html"><div class="menuopt">Mapa</div></a>\n' +
+    '<a href="quiz.html"><div class="menuopt">Quiz</div></a>\n' +
+    '<a href="Kontakt.html"><div class="menuopt">Kontakt</div></a>';
+let elem = document.querySelector(".menu");
+elem.innerHTML = navbar;
